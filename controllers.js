51a39d197d9e171e269260c55cfab7427e274e69
sophiacheong <sophiaacheong@gmail.com>
2021-09-14 21:36:40 -0700

    added sortBy and direction req query params

--- controllers.js
+++ controllers.js
@@ -9,7 +9,11 @@ const controllers = {
   get: (req, res) => {
     if (!req.query.tags) res.status(404).send({ error: "Tags parameter is required" });
     const tagsArr = req.query.tags.split(',');
-    const sort = req.query.sortBy
+    const sort = req.query.sortBy;
+    let desc;
+    if (req.query.direction) {
+      req.query.direction === 'desc' ? desc = true : desc = false;
+    }
     const allReq = [];
     tagsArr.forEach(el => allReq.push(axios.get(`${url}?tag=${el}`)));
     axios.all(allReq)
@@ -17,7 +21,8 @@ const controllers = {
         let temp = res.map(r => r.data);
         let sumOfPost = { post: [] };
         temp.map(el => el.posts.map(inner => sumOfPost.post.push(inner)));
-        if (sort) sumOfPost = { post: sumOfPost.post.sort((a, b) => a[sort] - b[sort])}
+        if (sort && desc) sumOfPost = { post: sumOfPost.post.sort((a, b) => b[sort] - a[sort])}
+        else if (sort && !desc) sumOfPost = { post: sumOfPost.post.sort((a, b) => a[sort] - b[sort])}
         return sumOfPost;
       })
       .then((sumOfPost) => res.status(200).send(sumOfPost))
